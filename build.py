6918f7524bf01703734712b297e72c0a879a2ff0
AndroidGeeksYT <xavierlibresanora@gmail.com>
2025-11-19 20:07:15 +0800

    build: rearrange python build script

--- build.py
+++ build.py
@@ -4,53 +4,43 @@ import subprocess
 import string
 import random
 
+# Generate a random temporary bash script name
 bashfile=''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
 bashfile='/tmp/'+bashfile+'.sh'
 
 f = open(bashfile, 'w')
 s = """#!/bin/bash
-# Telegram Config
+
+# NOTE: Telegram and Pixeldrain logic removed for cleaner CI integration.
+# The GitHub Actions workflow will handle logging and artifact uploads.
+
+# Telegram Config (REMOVED, KEEPING VARS UNUSED FOR SIMPLICITY)
 TOKEN=$(/usr/bin/env python -c "import os; print(os.environ.get('TOKEN'))")
 CHATID=$(/usr/bin/env python -c "import os; print(os.environ.get('CHATID'))")
 API_KEY=$(/usr/bin/env python -c "import os; print(os.environ.get('API_KEY'))")
-BOT_MSG_URL="https://api.telegram.org/bot${TOKEN}/sendMessage"
-BOT_BUILD_URL="https://api.telegram.org/bot${TOKEN}/sendDocument"
-# Build Machine details
+
+# Build Machine details (STILL USED FOR LOGGING)
 cores=$(lscpu | grep "Core(s) per socket" | awk '{print $NF}')
 os=$(cat /etc/issue)
 time=$(TZ="Asia/Dhaka" date "+%a %b %d %r")
-# send saxx msgs to tg
+
+# Placeholder functions for removed Telegram logic
 tg_post_msg() {
-  curl -s -X POST "$BOT_MSG_URL" -d chat_id="$CHATID" \\
-    -d "disable_web_page_preview=true" \\
-    -d "parse_mode=html" \\
-    -d text="$1"
+  echo "::notice::Build message: $1"
 }
-# send build to tg
+
 tg_post_build()
 {
-	#Post MD5Checksum alongwith for easeness
-	MD5CHECK=$(md5sum "$1" | cut -d' ' -f1)
-	#Show the Checksum alongwith caption
-	curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \\
-	-F chat_id="$CHATID"  \\
-	-F "disable_web_page_preview=true" \\
-	-F "parse_mode=Markdown" \\
-	-F caption="$2 | *MD5 Checksum : *\\`$MD5CHECK\\`"
+	echo "::notice::Build file upload requested: $1"
 }
 
 kernel_dir="${PWD}"
-#CCACHE=$(command -v ccache)
 objdir="${kernel_dir}/out"
 anykernel=$HOME/anykernel
-builddir="${kernel_dir}/build"
 ZIMAGE=$kernel_dir/out/arch/arm64/boot/Image.gz-dtb
 kernel_name="GEEK"
 KERVER=$(make kernelversion)
-COMMIT_HEAD=$(git log --oneline -1)
 zip_name="$kernel_name-$(date +"%d%m%Y-%H%M")-signed.zip"
-#TC_DIR=$HOME/tc/
-#CLANG_DIR=$TC_DIR/clang-r522817
 export CONFIG_FILE="vendor/violet-perf_defconfig"
 export ARCH="arm64"
 export SUBARCH="arm64"
@@ -58,28 +48,23 @@ export CC="clang"
 export LLVM="1"
 export LLVM_IAS="1"
 export CLANG_TRIPLE="aarch64-linux-gnu-"
-export CROSS_COMPILE=ARM32="arm-linux-gnueabi-"
+export CROSS_COMPILE_ARM32="arm-linux-gnueabi-"
 export LD="aarch64-linux-gnu-ld.bfd"
 export KBUILD_BUILD_HOST=debian
 export KBUILD_BUILD_USER=androidgeeks
 LINUX_COMPILE_BY="androidgeeks"
 LINUX_COMPILE_HOST="debian"
-#export PATH="$CLANG_DIR/bin:$PATH"
-export CI_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-tg_post_msg "<b>Kernel : </b><code>$kernel_name</code>%0A<b>Upstream Version : </b><code>$KERVER</code>%0A<b>Machine : </b><code>$os</code>%0A<b>Cores : </b><code>$cores</code>%0A<b>Time : </b><code>$time</code>%0A<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><code>$COMMIT_HEAD</code>"
-#if ! [ -d "$TC_DIR" ]; then
-#    echo "Toolchain not found! Cloning to $TC_DIR..."
-#    tg_post_msg "<code>Toolchain not found! Cloning toolchain</code>"
-#    if ! git clone -q --depth=1 --single-branch https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b master $TC_DIR; then
-#        echo "Cloning failed! Aborting..."
-#        exit 1
-#    fi
-#fi
+# Initial log message (using GitHub Actions logging commands for visibility)
+echo "::group::Build Setup and Info"
+echo "Kernel: $kernel_name"
+echo "Upstream Version: $KERVER"
+echo "Top Commit: $COMMIT_HEAD"
+echo "::endgroup::"
+
 # Colors
 NC='\\033[0m'
 RED='\\033[0;31m'
-LRD='\\033[1;31m'
 LGR='\\033[1;32m'
 make_defconfig()
 {
@@ -91,6 +76,7 @@ compile()
 {
     cd ${kernel_dir}
     echo -e ${LGR} "######### Compiling kernel #########${NC}"
+    # The 'tee' command captures all output to error.log for artifact upload
     make -j$(nproc --all) \\
     O=out \\
     ARCH=${ARCH}\\
@@ -101,49 +87,86 @@ compile()
     LLVM=1 \\
     LLVM_IAS=1 \\
     2>&1 | tee error.log
+    
+    # Check the exit status of the 'make' command
+    if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        echo "::error::Compilation failed. See error.log artifact."
+        return 1
+    fi
+    return 0
 }
 completion() {
   cd ${objdir}
   COMPILED_IMAGE=arch/arm64/boot/Image.gz-dtb
   COMPILED_DTBO=arch/arm64/boot/dtbo.img
-  if [[ -f ${COMPILED_IMAGE} && ${COMPILED_DTBO} ]]; then
+  
+  # Check for successful compilation files
+  if [[ -f ${COMPILED_IMAGE} && -f ${COMPILED_DTBO} ]]; then
+    echo "::group::Packaging Kernel ZIP"
     git clone -q https://github.com/AndroidGeeksYT/AnyKernel3 $anykernel
     mv -f $ZIMAGE ${COMPILED_DTBO} $anykernel
     cd $anykernel
-    find . -name "*.zip" -type f
+    
+    # Clean up old zips and create new one
     find . -name "*.zip" -type f -delete
     zip -r AnyKernel.zip *
-    # Sign the ZIP file using zipsigner-3.0.jar
+    
+    # Sign the ZIP file
     curl -sLo zipsigner-3.0.jar https://github.com/Magisk-Modules-Repo/zipsigner/raw/master/bin/zipsigner-3.0-dexed.jar
+    if ! command -v java &> /dev/null
+    then
+        echo "Java not found. Installing OpenJDK..."
+        # Install java to run zipsigner.jar
+        sudo apt-get install -y openjdk-17-jdk
+    fi
     java -jar zipsigner-3.0.jar AnyKernel.zip AnyKernel-signed.zip
+    
+    # Move and rename the final artifact to the $HOME directory
     mv AnyKernel-signed.zip $zip_name
     mv $anykernel/$zip_name $HOME/$zip_name
+    
+    # Clean up
     rm -rf $anykernel
+
+    # Calculate and display build time
     END=$(date +"%s")
     DIFF=$(($END - $START))
-    BUILDTIME=$(echo $((${END} - ${START})) | awk '{print int ($1/3600)" Hours:"int(($1/60)%60)"Minutes:"int($1%60)" Seconds"}')
     file_path="$HOME/$zip_name"
-    url="https://pixeldrain.com/u/$(curl -s -T "$file_path" -u :"$API_KEY" "https://pixeldrain.com/api/file/" | jq -r '.id')"
     zip_size=$(du -h $HOME/$zip_name | awk '{print $1}')
-    tg_post_msg "<b>File Name:</b> <code>$zip_name</code>%0A<b>File Size:</b> <code>$zip_size</code>%0A<b>Download Link:</b> <a href='${url}'>Click Here</a>%0A<b>Build Time: $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)</b>"
-    echo
-    echo -e ${LGR} "############################################"
+
+    echo "::notice::Kernel packaging successful."
+    echo "Final ZIP: $file_path (Size: $zip_size)"
+    echo "Build Time: $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
+    echo -e ${LGR} "\n############################################"
     echo -e ${LGR} "############# OkThisIsEpic!  ##############"
     echo -e ${LGR} "############################################${NC}"
+    echo "::endgroup::"
+
   else
-    tg_post_build "$kernel_dir/error.log" "$CHATID" "Debug Mode Logs"
+    echo "::error::Kernel image or DTBO file not found. Packaging failed."
+    exit 1 # Exit with error code so GitHub Actions captures the failure
+  fi
+}
+
+# --- Execution Flow ---
+if make_defconfig; then
+  tg_post_msg "<code>Defconfig generated successfully</code>"
+  if compile; then
+    completion
+  else
+    # Compile failed, ensure error.log is available
     tg_post_msg "<code>Compilation failed</code>"
     echo -e ${RED} "############################################"
     echo -e ${RED} "##         This Is Not Epic :'(           ##"
     echo -e ${RED} "############################################${NC}"
+    exit 1 # Important for GitHub Actions to mark the job as failed
   fi
-}
-make_defconfig
-if [ $? -eq 0 ]; then
-  tg_post_msg "<code>Defconfig generated successfully</code>"
+else
+    # Defconfig failed
+    echo "::error::Defconfig step failed. Aborting build."
+    exit 1
 fi
-compile
-completion
+
 cd ${kernel_dir}
 """
 f.write(s)
