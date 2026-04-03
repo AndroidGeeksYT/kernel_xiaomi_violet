d470bbbed9e9023ebd4c63220bfd4b82fdc32e06
AndroidGeeksYT <xavierlibresanora@gmail.com>
2026-04-04 00:34:57 +0800

    security: selinux: ss: add return 0
    - add return 0 audit_update_lsm_rules to avoid warning non-void function
    does not return a value.

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3558,7 +3558,10 @@ out:
 	return match;
 }
 
-int audit_update_lsm_rules(void){};
+int audit_update_lsm_rules(void)
+{
+	return 0;
+};
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
